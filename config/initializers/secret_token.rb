26c2aa243fa4cb3b16da3e2c2c6937888d8e2c46
Yossi Shalem <yossishalem@gmail.com>
2013-07-02 08:20:20 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LidiaKozokin::Application.config.secret_token = '330c4117a24f8649b086c1d78bf530450b640d8831615a358574002aa59002dcd379475571d7bf4fff9ed179a4bb381d28ffb7d3e9fec80fb34c33200391c684'
